96d1edb55193b5d569ec1b756f83900c2e31d925
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:12:01 +0700

    Initial commit: ModuleDanishLanguagePack language pack for MikoPBX

---
+++ Messages/en/ModuleDanishLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleDanishLanguagePack' => 'Danish Language Pack',
+    'SubHeaderModuleDanishLanguagePack' => 'Complete Danish language support for MikoPBX',
+];
---
+++ Messages/ru/ModuleDanishLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleDanishLanguagePack' => 'Языковой пакет - Датский',
+    'SubHeaderModuleDanishLanguagePack' => 'Комплексная поддержка датского языка для системы MikoPBX',
+];
